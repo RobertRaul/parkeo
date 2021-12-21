451d70549e8f35e7bf4960e8bcb7b0131028f5ee
Robert <lordnebr4@gmail.com>
2021-12-21 15:59:24 -0500

    Alistar el reporte de CAJA

--- app/Http/Controllers/Reportes/PDF.php
+++ app/Http/Controllers/Reportes/PDF.php
@@ -6,31 +6,35 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Codedge\Fpdf\Fpdf\Fpdf;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
 class PDF extends Fpdf
 {
-    private $titul;
+    private $titul,$caj_id,$user_id;
 
-    public function Tittle($titulo)
+    public function Cabecera($titulo,$caj_id,$user_id)
     {
         $this->titul =$titulo;
+        $this->caj_id =$caj_id;
+        $this->user_id =$user_id;
     }
     // Cabecera de página
     function Header()
     {
         // Logo
-        $this->Image(asset('images/parkeo/parking.png'), 10, 5, 20);        
+        $this->Image(asset('images/parkeo/parking.png'), 10, 5, 20);
         $this->SetFont('Arial', 'B', 15);
-                
-     
+
+
         $this->Cell(80);
-        $this->Cell(30,6,$this->titul,0,0,'C');       
-        $this->SetFont('Arial', '', 12); 
-        $this->Cell(85,6, 'Fecha:' . '20/12/2021',0,0,'R');
+        $this->Cell(30,6,$this->titul,0,0,'C');
+        $this->SetFont('Arial', '', 11);
+        $this->Cell(85,6, 'Fecha:' . Carbon::now()->format('d/m/Y'),0,0,'R');
         $this->Ln();
         $this->Cell(80);
-        $this->Cell(30,6,'Codigo: ' . '15 -' .'Usuario:'. Auth::id() ,0,0,'C');        
-        $this->Cell(85,6,'Hora:' . '15:33:12',0,0,'R');
-    
+        $this->Cell(30,6,'Codigo: ' . '15 -' .'Usuario:'. Auth::id() ,0,0,'C');
+        $this->Cell(85,6,'Hora:' . Carbon::now()->format('H:i:s'),0,0,'R');
+        // >format('H:i:s d/m/Y')
         // Salto de línea
         $this->Ln(20);
     }
--- app/Http/Controllers/Reportes/ReportCaja.php
+++ app/Http/Controllers/Reportes/ReportCaja.php
@@ -5,19 +5,68 @@ namespace App\Http\Controllers\Reportes;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Reportes\PDF;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\FuncCall;
 
-
-class ReportCaja extends Controller
+class ReportCaja extends PDF
 {
-    public function Prueba()
+
+    public function tabla($idcaja)
     {
-        $pdf = new PDF();
-        $pdf->AliasNbPages();
-        $pdf->Tittle('Reporte General de Caja');
-        $pdf->AddPage();
-        $pdf->SetFont('Arial', 'B', 16);
-        $pdf->Cell(40, 10, 'Hola, Mundo');
-        $pdf->Output();
-        exit;
+        $data = DB::table('ingresos as i')
+            ->join('cajas as c', 'c.caj_id', '=', 'i.ing_cajid')
+            ->join('rentas as r', 'r.rent_id', '=', 'i.ing_rentid')
+            ->join('series as s', 's.ser_id', '=', 'i.ing_serid')
+            ->join('clientes as cl', 'cl.clie_id', '=', 'r.rent_client')
+            ->join('vehiculos as v', 'v.veh_id', '=', 'r.rent_vehiculo')
+            ->join('cajones as ca', 'ca.caj_id', '=', 'r.rent_cajonid')
+            ->join('tarifas as t', 't.tar_id', '=', 'r.rent_tarid')
+            ->select(
+                'c.caj_feaper',
+                'i.ing_serie',
+                'i.ing_numero',
+                'cl.clie_nombres',
+                'i.ing_tppago',
+                'v.veh_placa',
+                't.tar_precio',
+                'r.rent_totalhoras',
+                'i.ing_nref',
+                'i.ing_total',
+                'i.ing_estado',
+                'i.ing_motivo',
+                'i.ing_fechr',
+                'ca.caj_desc'
+            )
+            ->where('c.caj_id', '=', $idcaja)
+            ->orderBy('i.ing_fechr')
+            ->get();
+
+        // Anchuras de las columnas los ANCHOS
+        $w = array(15, 18, 40, 20, 20, 20, 20, 20, 20, 25, 25, 30);
+        // Cabeceras
+        $cabecera = array('Serie', 'Numero', 'Cliente', 'Tp. Pago', 'Nro Ref', 'Placa', 'Cajon', 'Tarifa', 'Horas', 'Total', 'Estado', 'Motivo');
+        for ($i = 0; $i < count($cabecera); $i++)
+            $this->Cell($w[$i], 7, $cabecera[$i], 1, 0, 'C');
+        $this->Ln();
+        // Datos
+        $resultado = json_decode($data);
+        foreach ($resultado as $row) {
+            $this->Cell($w[0], 6, $row[0], 'LR');
+            $this->Cell($w[1], 6, $row[1], 'LR');
+            $this->Cell($w[2], 6, $row[2], 'LR', 0, 'R');
+            $this->Cell($w[3], 6, $row[3], 'LR', 0, 'R');
+            $this->Ln();
+        }
+        // Línea de cierre
+        $this->Cell(array_sum($w), 0, '', 'T');
     }
 }
+
+$pdf = new ReportCaja('L');
+$pdf->AliasNbPages();
+$pdf->Cabecera('Reporte General de Caja', '322', 'ROBERT PS');
+$pdf->AddPage();
+$pdf->SetFont('Arial', 'B', 11);
+$pdf->tabla(9);
+$pdf->Output();
+exit;
